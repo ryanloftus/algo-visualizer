906aa950cfb59825e8a7583b213f36c426550203
ryanloftus <ryanloftus02@outlook.com>
2022-07-03 13:43:28 -0400

    Added state for Navbar

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
-import Navbar from './Components/Navbar';
+import { Navbar } from './Components/Navbar';
+
+const algorithmTypes = ["Sort", "Search", "Find Path"];
 
 function App() {
-  return (
-    <div className="App w-screen h-screen bg-slate-800 flex">
-      <Navbar></Navbar>
-    </div>
-  );
+    const [algoTypeIdx, setAlgoTypeIdx] = useState(0);
+
+    const selectAlgoTypeByIdx = (idx: number) => {
+        setAlgoTypeIdx(idx);
+    }
+
+    return (
+        <div className="App w-screen h-screen bg-slate-800 flex">
+            <Navbar options={algorithmTypes} selectedOptionIdx={algoTypeIdx} selectOptionByIdx={selectAlgoTypeByIdx} />
+        </div>
+    );
 }
 
 export default App;
--- app/src/Components/Navbar.tsx
+++ app/src/Components/Navbar.tsx
@@ -1,9 +1,20 @@
-export default function Navbar() {
+import React from 'react';
+
+interface Props {
+    options: string[],
+    selectedOptionIdx: number,
+    selectOptionByIdx: (idx: number) => void,
+}
+
+const unselectedStyle: string = "block py-4 text-center align-middle hover:border-solid hover:border-b-2 hover:border-slate-50";
+const selectedStyle: string = unselectedStyle + " border-solid border-b-2 border-slate-300 text-slate-300";
+
+export const Navbar : React.FC<Props> = (props: Props) => {
     return (
-       <nav className="bg-gradient-to-r from-sky-700 to-sky-900 space-x-4 text-xl text-slate-50 grid grid-flow-col grow h-max">
-        <a className="block py-4 text-center align-middle hover:border-solid hover:border-b-2 hover:border-slate-50">Sorting</a>
-        <a className="block py-4 text-center align-middle hover:border-solid hover:border-b-2 hover:border-slate-50">Path Finding</a>
-        <a className="block py-4 text-center align-middle hover:border-solid hover:border-b-2 hover:border-slate-50">Searching</a>
-       </nav>
+        <nav className="bg-gradient-to-r from-sky-700 to-sky-900 space-x-4 text-xl text-slate-50 grid grid-flow-col grow h-max">
+            {props.options.map((v, i) => 
+                <a key={i} onClick={() => props.selectOptionByIdx(i)} className={i === props.selectedOptionIdx ? selectedStyle : unselectedStyle}>{v}</a>
+            )}
+        </nav>
     );
 }
